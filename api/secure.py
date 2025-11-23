c8511f4a22115c7e5f01cedfc18708e004f4b332
tingisgood <83741278+tingisgood@users.noreply.github.com>
2025-11-23 23:45:58 +0800

    Update secure.py

--- api/secure.py
+++ api/secure.py
@@ -1,11 +1,12 @@
 # secure.py
 
 from fastapi import APIRouter, Depends
-from middlewares.auth import verify_token
+from api.middlewares.auth import verify_token
 
 router = APIRouter()
 
 @router.get("/profile")
 def get_profile(user = Depends(verify_token)):
     return {"uid": user["uid"], "email": user.get("email")}
+
 
