a5128eda71c198fcd94e970812e6439ca7912c22
tingisgood <83741278+tingisgood@users.noreply.github.com>
2025-11-24 02:15:21 +0800

    Update firebase.py

--- api/firebase.py
+++ api/firebase.py
@@ -1,10 +1,23 @@
 #firebase.py
+# import firebase_admin
+# from firebase_admin import credentials, auth, initialize_app
+# import os
+
+# key_path = os.path.join(os.path.dirname(__file__), "firebase_key.json")
+# # 讀取 Firebase Admin 金鑰
+# cred = credentials.Certificate(key_path)
+# firebase_admin.initialize_app(cred)
+
 import firebase_admin
-from firebase_admin import credentials, auth, initialize_app
+from firebase_admin import credentials, auth
 import os
 
-key_path = os.path.join(os.path.dirname(__file__), "firebase_key.json")
-# 讀取 Firebase Admin 金鑰
-cred = credentials.Certificate(key_path)
-firebase_admin.initialize_app(cred)
+cred_path = os.path.join(os.path.dirname(__file__), "firebase_key.json")
+cred = credentials.Certificate(cred_path)
+
+# ✅ 確保只初始化一次
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred)
+
+
 
